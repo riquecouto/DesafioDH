4d36820a231c13064838b1cda428a974b1820585
Henrique <henriquecouto@poli.ufrj.br>
2020-09-29 13:43:05 -0300

    Conclusão Parte I

--- src/br/com/digitalhouse/desafio/DigitalHouseManager.kt
+++ src/br/com/digitalhouse/desafio/DigitalHouseManager.kt
@@ -52,12 +52,54 @@ class DigitalHouseManager{
         println("Professor excluído")
     }
 
-    fun registrarAluno(nome: String, sobrenome: String, codigoAluno:Int) {
+    fun registrarAluno(nome: String, sobrenome: String, codigoAluno: Int) {
         if (listaDeAlunos.find {it.codigoAluno == codigoAluno} != null) {
             println("Aluno já existe")
             return
         }
         listaDeAlunos.add(Aluno(codigoAluno, nome, sobrenome))
         println("Aluno registrado com sucesso")
+    }
+
+    fun matricularAluno(codigoAluno: Int, codigoCurso: Int) {
+        val aluno = listaDeAlunos.find {it.codigoAluno == codigoAluno}
+        val curso = listaDeCursos.find {it.codigoCurso == codigoCurso}
+
+        if (aluno == null) {
+            println("Aluno não existe")
+            return
+        }
+        if (curso == null) {
+            println("Curso não existe")
+            return
+        }
+        if (curso.listaDeAlunos.size == curso.quantidadeMaximaDeAlunos) {
+            println("Não há vagas disponíves")
+            return
+        }
+        if (curso.adicionarUmAluno(aluno))
+            println("Matrícula efetuada com sucesso")
+        else
+            println("Aluno já matriculado")
+    }
+
+    fun alocarProfessores(codigoCurso: Int,codigoProfessorTitular:  Int, codigoProfessorAdjunto:Int) {
+        val professorTitular = listaDeProfessores.find {it.codigoProfessor == codigoProfessorTitular}
+        val professorAdjunto = listaDeProfessores.find {it.codigoProfessor == codigoProfessorAdjunto}
+        val curso = listaDeCursos.find {it.codigoCurso == codigoCurso}
+
+        if (professorTitular == null) {
+            println("Professor titular não existe")
+            return
+        }
+        if (professorAdjunto == null) {
+            println("Professor adjunto não existe")
+            return
+        }
+        if (curso == null) {
+            println("Curso não existe")
+            return
+        }
+        curso.professorTitular = professorTitular as ProfessorTitular
     }
 }
