b6486198e84ff95fe92fe2753a8ab9342d1faf2b
Henrique <henriquecouto@poli.ufrj.br>
2020-09-29 14:30:42 -0300

    Parte J e melhoria das mensagens

--- src/br/com/digitalhouse/desafio/DigitalHouseManager.kt
+++ src/br/com/digitalhouse/desafio/DigitalHouseManager.kt
@@ -9,56 +9,56 @@ class DigitalHouseManager{
 
     fun registrarCurso(nome: String, codigoCurso: Int, quantidadeMaximaDeAlunos: Int) {
         if (listaDeCursos.find {it.codigoCurso == codigoCurso} != null) {
-            println("Curso já existe")
+            println("Curso $codigoCurso já existe")
             return
         }
         listaDeCursos.add(Curso(codigoCurso, nome, quantidadeMaximaDeAlunos))
-        println("Curso registrado com sucesso")
+        println("Curso $codigoCurso registrado com sucesso")
     }
 
     fun excluirCurso(codigoCurso: Int) {
         if (listaDeCursos.find {it.codigoCurso == codigoCurso} == null) {
-            println("Curso não existe")
+            println("Curso $codigoCurso não existe")
             return
         }
         listaDeCursos.removeAll {it.codigoCurso == codigoCurso}
-        println("Professor excluído")
+        println("Curso $codigoCurso excluído")
     }
 
     fun registrarProfessorAdjunto(nome: String, sobrenome: String, codigoProfessor: Int, quantidadeDeHoras: Int) {
         if (listaDeProfessores.find {it.codigoProfessor == codigoProfessor} != null) {
-            println("Professor já existe")
+            println("Professor $codigoProfessor já existe")
             return
         }
         listaDeProfessores.add(ProfessorAdjunto(codigoProfessor, nome, sobrenome, 0, quantidadeDeHoras))
-        println("Professor adjunto registrado com sucesso")
+        println("Professor adjunto $codigoProfessor registrado com sucesso")
     }
 
     fun registrarProfessorTitular(nome: String, sobrenome: String, codigoProfessor: Int, especialidade: String) {
         if (listaDeProfessores.find {it.codigoProfessor == codigoProfessor} != null) {
-            println("Professor já existe")
+            println("Professor $codigoProfessor já existe")
             return
         }
         listaDeProfessores.add(ProfessorTitular(codigoProfessor, nome, sobrenome, 0, especialidade))
-        println("Professor titular registrado com sucesso")
+        println("Professor titular $codigoProfessor registrado com sucesso")
     }
 
     fun excluirProfessor(codigoProfessor: Int) {
         if (listaDeProfessores.find {it.codigoProfessor == codigoProfessor} == null) {
-            println("Professor não existe")
+            println("Professor $codigoProfessor não existe")
             return
         }
         listaDeProfessores.removeAll {it.codigoProfessor == codigoProfessor}
-        println("Professor excluído")
+        println("Professor $codigoProfessor excluído")
     }
 
     fun registrarAluno(nome: String, sobrenome: String, codigoAluno: Int) {
         if (listaDeAlunos.find {it.codigoAluno == codigoAluno} != null) {
-            println("Aluno já existe")
+            println("Aluno $codigoAluno já existe")
             return
         }
         listaDeAlunos.add(Aluno(codigoAluno, nome, sobrenome))
-        println("Aluno registrado com sucesso")
+        println("Aluno $codigoAluno registrado com sucesso")
     }
 
     fun matricularAluno(codigoAluno: Int, codigoCurso: Int) {
@@ -66,40 +66,43 @@ class DigitalHouseManager{
         val curso = listaDeCursos.find {it.codigoCurso == codigoCurso}
 
         if (aluno == null) {
-            println("Aluno não existe")
+            println("Aluno $codigoAluno não existe")
             return
         }
         if (curso == null) {
-            println("Curso não existe")
+            println("Curso $codigoCurso não existe")
             return
         }
         if (curso.listaDeAlunos.size == curso.quantidadeMaximaDeAlunos) {
-            println("Não há vagas disponíves")
+            println("Não há vagas disponíves no curso $codigoCurso")
             return
         }
         if (curso.adicionarUmAluno(aluno))
-            println("Matrícula efetuada com sucesso")
+            println("Matrícula do aluno $codigoAluno no curso $codigoCurso efetuada com sucesso")
         else
-            println("Aluno já matriculado")
+            println("Aluno $codigoAluno já está matriculado no no curso $codigoCurso")
     }
 
-    fun alocarProfessores(codigoCurso: Int,codigoProfessorTitular:  Int, codigoProfessorAdjunto:Int) {
-        val professorTitular = listaDeProfessores.find {it.codigoProfessor == codigoProfessorTitular}
-        val professorAdjunto = listaDeProfessores.find {it.codigoProfessor == codigoProfessorAdjunto}
+    fun alocarProfessores(codigoCurso: Int, codigoProfessorTitular: Int, codigoProfessorAdjunto: Int) {
+        val professorTitular = listaDeProfessores.find {it is ProfessorTitular && it.codigoProfessor == codigoProfessorTitular}
+        val professorAdjunto = listaDeProfessores.find {it is ProfessorAdjunto && it.codigoProfessor == codigoProfessorAdjunto}
         val curso = listaDeCursos.find {it.codigoCurso == codigoCurso}
 
         if (professorTitular == null) {
-            println("Professor titular não existe")
+            println("Professor titular $codigoProfessorTitular não existe")
             return
         }
         if (professorAdjunto == null) {
-            println("Professor adjunto não existe")
+            println("Professor adjunto $codigoProfessorAdjunto não existe")
             return
         }
         if (curso == null) {
-            println("Curso não existe")
+            println("Curso $codigoCurso não existe")
             return
         }
+
         curso.professorTitular = professorTitular as ProfessorTitular
+        curso.professorAdjunto = professorAdjunto as ProfessorAdjunto
+        println("Professores $codigoProfessorTitular e $codigoProfessorAdjunto alocados ao curso $codigoCurso")
     }
 }
---
+++ src/br/com/digitalhouse/desafio/Main.kt
@@ -0,0 +1,29 @@
+package br.com.digitalhouse.desafio
+
+fun main() {
+    val manager = DigitalHouseManager()
+
+    manager.registrarProfessorTitular("Ana", "Silva", 101, "Android")
+    manager.registrarProfessorTitular("Paulo", "Souza", 102, "SQL")
+
+    manager.registrarProfessorAdjunto("Célia", "Matos", 201, 500)
+    manager.registrarProfessorAdjunto("Heitor", "Rios", 202, 600)
+
+    manager.registrarCurso("Full Stack", 20001, 3)
+    manager.registrarCurso("Android", 20002, 2)
+
+    manager.alocarProfessores(20001, 101, 201)
+    manager.alocarProfessores(20002, 102, 202)
+
+    manager.registrarAluno("Henrique", "Couto", 301)
+    manager.registrarAluno("Maria", "Joaquina", 302)
+    manager.registrarAluno("Igor", "Pereira", 303)
+    manager.registrarAluno("Joana", "Almeida", 304)
+
+    manager.matricularAluno(301, 20001)
+    manager.matricularAluno(302, 20001)
+
+    manager.matricularAluno(302, 20002)
+    manager.matricularAluno(303, 20002)
+    manager.matricularAluno(304, 20002)
+}
